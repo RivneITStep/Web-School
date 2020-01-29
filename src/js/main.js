ebcfd156e0073501edab808b2bcc70b0592d4e8d
Evgeniy Kamenchuk <unnamed-jkl@rivne.itstep.org>
2020-01-29 18:38:46 +0200

    header

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,26 @@
-//= test
+window.onscroll = function () {
+    if (window.pageYOffset > 5) {
+        document.getElementById('for-b').style.top = 0;
+        document.querySelector('.logo-fixed').style.display = "block";
+    } else {
+        document.getElementById('for-b').style.top = 50 + 'px';
+        document.querySelector('.logo-fixed').style.display = "none";
+    }
+};
+window.onload = function () {
+    if (document.getElementById('burger').checked == true) {
+        document.getElementById('#span-one').style.rotate = 584 + "deg";
+    }
+}
+$('.sl').slick({
+    autoplay: true,
+    autoplaySpeed: 1000,
+    speed: 1000,
+    cssEase: 'ease-in',
+    // centerMode: true,
+    centerPadding: '25px',
+    dots: true,
+    arrows: false,
+    // pauseOnDotsHover: true,
+    // adaptiveHeight: true
+});
