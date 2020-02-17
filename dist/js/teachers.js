3e8ac314fd3fa4678dac9d47b4c33dd66295b3c3
natalysiyah <natasha.artyuh@gmail.com>
2020-02-17 14:33:49 +0200

    fix resize

--- dist/js/teachers.js
+++ dist/js/teachers.js
@@ -3,7 +3,7 @@ let moreBlock=document.getElementById("div1");
 function down() {
     if(moreText.style.display="none"){
         moreText.style.display="block";
-        moreBlock.style.height="370px";
+        // moreBlock.style.height="370px";
         document.getElementById("button1").style.display="none";
         document.getElementById("button2").style.display="block";
     }
@@ -12,7 +12,7 @@ function down() {
   function up() {
   if(moreText.style.display="block"){
     moreText.style.display="none";
-    moreBlock.style.height="200px";
+    // moreBlock.style.height="200px";
     document.getElementById("button2").style.display="none";
         document.getElementById("button1").style.display="block";
 }
