15a0a656e88fff184ca52ed9ae6c0ed49eb38fab
Nataliya Kulik <siyah@rivne.itstep.org>
2020-02-26 20:54:27 +0200

    jquery

--- dist/js/teachers.js
+++ dist/js/teachers.js
@@ -1,19 +1,35 @@
-let moreText=document.getElementById("hidden-text");
-let moreBlock=document.getElementById("div1");
-function down() {
-    if(moreText.style.display="none"){
-        moreText.style.display="block";
-        // moreBlock.style.height="370px";
-        document.getElementById("button1").style.display="none";
-        document.getElementById("button2").style.display="block";
-    }
-        
-  };
-  function up() {
-  if(moreText.style.display="block"){
-    moreText.style.display="none";
-    // moreBlock.style.height="200px";
-    document.getElementById("button2").style.display="none";
-        document.getElementById("button1").style.display="block";
-}
-  };
+// let moreText = document.getElementById("hidden-text");
+// // let moreBlock=document.getElementById("div1");
+// let btnToggle = document.querySelector("#button1");
+// btnToggle.addEventListener("click", down);
+
+// function down(btnToggle) {
+//   console.log("This = > ", btnToggle);
+//   if ((moreText.style.display = "none")) {
+//     moreText.style.display = "block";
+//     // moreBlock.style.height="370px";
+//     document.getElementById("button1").style.display = "none";
+//     document.getElementById("button2").style.display = "block";
+//   }
+// }
+// function up() {
+//   console.log("This = > ", this);
+//   if ((moreText.style.display = "block")) {
+//     moreText.style.display = "none";
+//     // moreBlock.style.height="200px";
+//     document.getElementById("button2").style.display = "none";
+//     document.getElementById("button1").style.display = "block";
+//   }
+// }
+$(document).ready(function() {
+  $("button").click(function() {
+    $(".hid").toggle(this);
+  });
+});
+// if ($(this).val() == "Детальніше") {
+//   $(".hid").toggle();
+//   $(this).val("Згорнути");
+// } else {
+//   $(".hid").toggle();
+//   $(this).val("Детальніше");
+// }
