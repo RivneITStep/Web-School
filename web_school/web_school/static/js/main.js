6183e3a3d585dc85c50f91d099cc11b4c1f4bdf6
Artemosypchuk <tyoska111@ukr.net>
2020-05-11 16:02:25 +0300

    'backend_v_0.1'

---
+++ web_school/web_school/static/js/main.js
@@ -0,0 +1,2 @@
+window.onscroll=function(){5<window.pageYOffset?(document.getElementById("for-b").style.top=0,document.querySelector(".logo-fixed").style.display="block"):(document.getElementById("for-b").style.top="50px",document.querySelector(".logo-fixed").style.display="none")},window.onload=function(){1==document.getElementById("burger").checked&&(document.getElementById("#span-one").style.rotate="584deg")},$(".sl").slick({autoplay:!0,autoplaySpeed:3e3,speed:1e3,cssEase:"ease-in",centerPadding:"25px",dots:!0,arrows:!1,adaptiveHeight:!0});
+//# sourceMappingURL=main.js.map
