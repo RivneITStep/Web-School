210db3e49059dceb4e03b41d6554a9589aca76d7
Artemosypchuk <tyoska111@ukr.net>
2020-05-11 18:15:53 +0300

    'backend_v_0.2'

--- web_school/pages/urls.py
+++ web_school/pages/urls.py
@@ -4,4 +4,10 @@ app_name = 'pages'
 
 urlpatterns = [
         path('', views.index, name="index"),
+        path('about/', views.about, name="about"),
+        path('courses/', views.courses, name="courses"),
+        path('contacts/', views.contacts, name="contacts"),
+        path('teachers/', views.teachers, name="teachers"),
+        path('blog/', views.blog, name="blog"),
+        path('testimonials/', views.testimonials, name="testimonials"),
 ]
--- web_school/pages/views.py
+++ web_school/pages/views.py
@@ -1,5 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def index(request):
     
     return render(request, 'pages/index.html')
+
+
+def about(request):
+    
+    return render(request, 'pages/about.html')
+
+
+def courses(request):
+    
+    return render(request, 'pages/courses.html')
+
+
+def contacts(request):
+    
+    return render(request, 'pages/contacts.html')
+
+
+def teachers(request):
+    
+    return render(request, 'pages/teachers.html')
+
+
+def blog(request):
+    
+    return render(request, 'pages/blog.html')
+
+
+def testimonials(request):
+    
+    return render(request, 'pages/testimonials.html')
